97e9a541083b86b4d5f52445693eb63ef151f9d5
0TheLearner0 <zishankhan.00f@gmail.com>
2026-05-18 18:12:04 +0530

    Uploading the first file of python demo

---
+++ demo.py
@@ -0,0 +1,12 @@
+
+Str1=input("Enter name: ")
+mango=input("Enter mango quantity: ")
+mango=int(mango)
+apple=input("Enter Apple quantity: ")
+apple=int(apple)
+sm=mango+apple
+# f-String has been used here
+print(f" {Str1} has {mango} mangoes and {apple} apples.")
+
+print("Fruits quantity: ",sm)
+
